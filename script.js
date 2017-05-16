013f2a9c4bafecbee44546879c0cebc1c23d1fd4
sharepopindonesia <ilhamalakbar001@gmail.com>
2017-05-16 19:52:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  // transisi image pertama
+  $('#first').delay(3000).fadeOut('fast');
+
+  // transisi image kedua
+  $('#notif').delay(3190).fadeIn('fast');      
+
+});
